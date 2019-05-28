8f6885ab2ddfaaa18d5ece525609a6ee162b3cff
lschimm <lschimmelfennig@gmail.com>
2019-05-27 19:12:07 -0500

    continuing part I

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,13 @@
 import React from 'react';
 import './Header.css';
+
+
+function HeaderContainer () {
+    return (
+      <div>
+        <h1>butts</h1>
+      </div>
+    );
+  }
+
+  export default HeaderContainer;
